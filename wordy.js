e3fc5eb61b7a9a43ce01afaca86323de7c46f7f2
Cara Hill <cara@rabidtech.co.nz>
2018-11-19 15:10:22 +1300

    Tests 4 to 7 passing.

--- wordy.js
+++ wordy.js
@@ -3,8 +3,21 @@ function WordProblem(question) {
 
   this.answer = () => {
     let result = this.question.match(/-?\d+/g).map(n => parseInt(n));
+
     if(question.includes("plus")) {
-      return result.reduce((a, b) => a + b)
+      return result.reduce((a, b) => a + b);
+    }
+
+    if(question.includes("minus")) {
+      return result.reduce((a, b) => a - b);
+    }
+
+    if(question.includes("multiplied")) {
+      return result.reduce((a, b) => a * b);
+    }
+
+    if(question.includes("divided")) {
+      return result.reduce((a, b) => a / b);
     }
   }
 }
